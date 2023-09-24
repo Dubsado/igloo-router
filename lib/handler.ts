0f5d161bb14040763a5da91dbdef57895d7eff40
Jake <jake@xyzdigital.com>
2023-09-24 10:21:15 -0600

    pass method into handler findRoute call

--- lib/handler.ts
+++ lib/handler.ts
@@ -1,10 +1,10 @@
+import { HTTPMethods } from '.'
 import { findRoute } from '../lib/router/findRoute'
 
 export const handler = async (req: Request) => {
     // URL to parse
     const myURL = new URL(req.url)
-
-    const route = findRoute(myURL.pathname)
+    const route = findRoute(myURL.pathname, req.method as HTTPMethods)
     if (!route) {
         return new Response(
             `Path: '${myURL.pathname}' not found in the Igloo Router`,
