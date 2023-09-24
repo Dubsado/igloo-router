d64751997abcd473641bf172e8306335c06f284d
Jake <jake@xyzdigital.com>
2023-09-24 07:29:23 -0600

    updates tests to cover new method API

--- lib/router/router.spec.ts
+++ lib/router/router.spec.ts
@@ -3,8 +3,8 @@ import { expect, test, describe } from 'bun:test'
 
 describe('Router', () => {
     test('should add static route and find handler', () => {
-        addRoute('/static', [], 'staticHandler')
-        const result = findRoute('/static')
+        addRoute(['GET'], '/static', [], 'staticHandler')
+        const result = findRoute('/static', 'GET')
         expect(result).toEqual({
             handler: 'staticHandler',
             middleware: [],
@@ -13,8 +13,8 @@ describe('Router', () => {
     })
 
     test('should add dynamic route and find handler', () => {
-        addRoute('/dynamic/:dynamic', [], 'dynamicHandler')
-        const result = findRoute('/dynamic/12345')
+        addRoute(['GET'], '/dynamic/:dynamic', [], 'dynamicHandler')
+        const result = findRoute('/dynamic/12345', 'GET')
         expect(result).toEqual({
             handler: 'dynamicHandler',
             middleware: [],
@@ -22,8 +22,22 @@ describe('Router', () => {
         })
     })
 
+    test('adding multiple routes at the same dynamic path should fail', () => {
+        addRoute(['GET'], '/dynamic2/:dynamic', [], 'dynamicHandler')
+        expect(() => {
+            addRoute(['GET'], '/dynamic2/:dynamic2', [], 'dynamicHandler2')
+        }).toThrow()
+    })
+
+    test('adding multiple routes at the same exact path should fail', () => {
+        addRoute(['GET'], '/static/path', [], () => {})
+        expect(() => {
+            addRoute(['GET'], '/static/path', [], () => {})
+        }).toThrow()
+    })
+
     test('should return null for an unknown route', () => {
-        const result = findRoute('/unknown')
+        const result = findRoute('/unknown', 'GET')
         expect(result).toBeNull()
     })
 })
