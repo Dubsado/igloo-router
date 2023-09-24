f65ec7e08d815fe7917c7cfd1ee7afa087f1da2d
Jake <jake@xyzdigital.com>
2023-09-24 10:20:57 -0600

    update examples to use new api

--- examples/basic-routing.ts
+++ examples/basic-routing.ts
@@ -1,38 +1,41 @@
-import { handler, addRoute } from '../lib/'
+import { handler, router } from '../lib/'
 import { logRequest } from './middleware/log-request'
 
-const healthHandler = (req: Request) => {
+router.get('/health', logRequest, (req: Request) => {
     return new Response('hello')
-}
-addRoute(['GET'], '/health', [logRequest], healthHandler)
+})
 
-const randomJsonHandler = (req: Request, params: { id: string }) => {
-    return new Response(
-        JSON.stringify({
-            success: true,
-            id: params.id,
-        })
-    )
-}
-addRoute(['GET'], '/random-json/:id', [logRequest], randomJsonHandler)
-/**
- * Spark up this server.
- */
+router.get(
+    '/random-json/:id',
+    logRequest,
+    (req: Request, params: { id: string }) => {
+        return new Response(
+            JSON.stringify({
+                success: true,
+                id: params.id,
+            })
+        )
+    }
+)
+
 const server = Bun.serve({
     port: 3000,
     fetch: handler,
 })
 
 console.log(`
-d8b        888                 
-Y8P        888           http://localhost:${server.port}      
-           888                 
-888 .d88b. 888 .d88b.  .d88b.  
-888d88P"88b888d88""88bd88""88b 
-888888  888888888  888888  888 
-888Y88b 888888Y88..88PY88..88P 
-888 "Y88888888 "Y88P"  "Y88P"  
-        888                    
-   Y8b d88P                    
-    "Y88P"                     
+    d8b        888                 
+    Y8P        888           http://localhost:${server.port}      
+            888                 
+    888 .d88b. 888 .d88b.  .d88b.  
+    888d88P"88b888d88""88bd88""88b 
+    888888  888888888  888888  888 
+    888Y88b 888888Y88..88PY88..88P 
+    888 "Y88888888 "Y88P"  "Y88P"  
+            888                    
+    Y8b d88P                    
+        "Y88P"                     
 `)
+
+console.log('')
+console.log('Logs: ', new Date().toLocaleString())
--- examples/list-routes.ts
+++ examples/list-routes.ts
@@ -1,5 +1,5 @@
-import { handler, addRoute } from '../lib/'
-import { printRoutes } from '../lib/router/listRoutes'
+import { handler, router, root } from '../lib/'
+import { printRoutes, listRoutes } from '../lib/utils/listRoutes'
 import { logRequest } from './middleware/log-request'
 
 const randomHandler = (req: Request, params: { id: string }) => {
@@ -12,8 +12,46 @@ const randomHandler = (req: Request, params: { id: string }) => {
     )
 }
 
-addRoute(['GET'], '/random/:id', [logRequest], randomHandler)
-addRoute(['GET'], '/random/things/do/work', [logRequest], randomHandler)
+router.get('/random/:id', logRequest, randomHandler)
+router.get('/random/things/do/work', logRequest, randomHandler)
+router.get('/', logRequest, () => {
+    return new Response(
+        `
+    <html>
+        <p>Hello, Bonjour, Hola, Привет, こんにちは, Olá</p>
+        <p>This example will show you the power of being able to see what 
+            dang routes you actually have on the server</p>
+        ${Routes()}
+    </html>
+    `,
+        {
+            headers: {
+                'Content-Type': 'text/html; charset=utf-8',
+            },
+        }
+    )
+})
+
+const routes = listRoutes(root)
+const Routes = () => {
+    const inner = routes
+        .map((route) => {
+            if (
+                route.method === 'GET' &&
+                !route.isDynamic &&
+                route.path !== '/'
+            ) {
+                return `<li>${route.method} - <a href="${route.path}">${route.path}</a></li>`
+            }
+            return `<li>${route.method} - ${route.path}</li>`
+        })
+        .join('')
+    return `
+        <ul>
+            ${inner}
+        </ul>
+    `
+}
 
 const server = Bun.serve({
     port: 3000,
@@ -21,17 +59,20 @@ const server = Bun.serve({
 })
 
 console.log(`
-d8b        888                 
-Y8P        888           http://localhost:${server.port}      
-           888                 
-888 .d88b. 888 .d88b.  .d88b.  
-888d88P"88b888d88""88bd88""88b 
-888888  888888888  888888  888 
-888Y88b 888888Y88..88PY88..88P 
-888 "Y88888888 "Y88P"  "Y88P"  
-        888                    
-   Y8b d88P                    
-    "Y88P"                     
+    d8b        888                 
+    Y8P        888           http://localhost:${server.port}      
+            888                 
+    888 .d88b. 888 .d88b.  .d88b.  
+    888d88P"88b888d88""88bd88""88b 
+    888888  888888888  888888  888 
+    888Y88b 888888Y88..88PY88..88P 
+    888 "Y88888888 "Y88P"  "Y88P"  
+            888                    
+    Y8b d88P                    
+        "Y88P"                     
 `)
 
 printRoutes()
+
+console.log('')
+console.log('Logs: ', new Date().toLocaleString())
